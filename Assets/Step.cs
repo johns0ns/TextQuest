8ac2b40d2b0133cb19ff00704ba5c9f841bb0c5e
Artem <krekotnev@gmail.com>
2020-02-06 21:19:51 +0300

    lesson4

---
+++ Assets/Step.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Step : MonoBehaviour
+{
+
+    [TextArea(minLines: 10, maxLines: 15)] public string content = "Перед вами 2 Двери:   \n 1.Первая дверь \n 2.Вторая дверь";
+
+    //public Step nextStep1;
+    //public Step nextStep2;
+    public Step[] nextSteps;
+    public Sprite Image;
+   
+}
--- Assets/TextQuestGame.cs
+++ Assets/TextQuestGame.cs
@@ -8,24 +8,50 @@ public class TextQuestGame : MonoBehaviour
     [Header("Elements")]
     public Text titleText;
     public Text gametext;
+    public Image gameImage;
 
     [Header("Config")]
-    [Tooltip("Название игры")] public string title = "Game Title";
-    //Просто так :)
-    [TextArea(minLines:10,maxLines:15)] public string content = "Перед вами 2 выхода:   \n 1.Выход один \n 2.Выход два \n 3.Выйти";
+    [Tooltip("Название игры")] public string title = "Начнем пожалуй!";
+    public Step activeStep;
     
     void Start()
     {
         titleText.text = title;
-        gametext.text = content;
-       
+        gametext.text = activeStep.content;
 
+
+        //string[] dayOfTheWeek = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+        //Debug.Log(dayOfTheWeek[5]);
+        //Debug.Log(dayOfTheWeek.Length);
     }
 
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            CheckPress(0);
+            gameImage.sprite = activeStep.Image;
+
+           
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            CheckPress(1);
+            gameImage.sprite = activeStep.Image;
+        }
         
+    }
+
+    void CheckPress(int index)
+    {
+        if (activeStep.nextSteps.Length > index)
+        {
+            activeStep = activeStep.nextSteps[index];
+            gametext.text = activeStep.content;
+        }
+
+
     }
 }
